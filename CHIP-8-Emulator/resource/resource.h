9ce51223d40b331f7585e3a95abceeace7796b58
willwill <jlwu.will@gmail.com>
2025-11-10 00:46:27 +0800

    config ui resource

--- CHIP-8-Emulator/resource/resource.h
+++ CHIP-8-Emulator/resource/resource.h
@@ -2,9 +2,17 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDD_DIALOG1                     101
 #define IDD_WINDOW                      101
+#define IDD_CONFIG                      101
 #define IDC_RENDER_AREA                 1001
+#define IDC_COMBO1                      1004
+#define IDC_CHECK1                      1005
+#define IDC_CHECK2                      1006
+#define IDC_CHECK3                      1007
+#define IDC_CHECK4                      1008
+#define IDC_CHECK5                      1009
+#define IDC_CHECK6                      1010
+#define IDC_SLIDER1                     1011
 
 // Next default values for new objects
 // 
@@ -12,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
